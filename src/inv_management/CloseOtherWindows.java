23251a0b9509cc823fd5a4c395cfea824522eb96
hunglviet <hunglviet@yahoo.com.vn>
2015-11-20 12:20:10 +0700

    Initial commit Signed-off-by:hunglviet <hunglviet@yahoo.com.vn>

---
+++ src/inv_management/CloseOtherWindows.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inv_management;
+
+import java.awt.Window;
+
+/**
+ *
+ * @author lvhung
+ */
+public class CloseOtherWindows {
+
+    public void closeWin() {
+        Window win[] = java.awt.Window.getWindows();
+        //
+        for (int i = 1; i < win.length; i++) { //i=0 - dong het cac cua so dang mo; i=1 - dong các cua so dang mo, tru cua so mo dau tien
+            win[i].dispose();
+            win[i] = null;
+        }
+    
+    }
+
+}
